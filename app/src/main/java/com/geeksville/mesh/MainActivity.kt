939436bc1051e09aceddf6ea8f5e1b86c069a95c
ESPIE <remi.espie@etu.umontpellier.fr>
2024-11-15 16:39:00 +0100

    feat: add sharing view

--- app/src/main/java/com/geeksville/mesh/MainActivity.kt
+++ app/src/main/java/com/geeksville/mesh/MainActivity.kt
@@ -307,6 +307,7 @@ class MainActivity : AppCompatActivity(), Logging {
                         }
                     } catch (e: SerializationException) {
                         debug("Failed to decode JSON: ${e.message}; falling back to default message")
+                        shareMessages(text)
                     }
                 }
             }
@@ -644,6 +645,13 @@ class MainActivity : AppCompatActivity(), Logging {
         model.setCurrentTab(0)
         if (contactKey != null && contactName != null && message != null) {
             supportFragmentManager.navigateToPreInitMessages(contactKey, contactName, message)
+        }
+    }
+
+    private fun shareMessages(message: String?) {
+        model.setCurrentTab(0)
+        if (message != null) {
+            supportFragmentManager.navigateToShareMessage(message)
         }
     }
 
